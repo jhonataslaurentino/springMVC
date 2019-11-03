e72bc2aaf2fa999d2b194775e5b2917c66ae6e4e
Jhonatas <costajhonatas9@gmail.com>
2019-11-03 08:13:53 +0000

    insercao de comentarios no customer

--- src/net/codejava/customer/Customer.java
+++ src/net/codejava/customer/Customer.java
@@ -1,63 +1,46 @@
 package net.codejava.customer;
- 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
- 
-@Entity
-@Table(name="customer")
-public class Customer {
 
+@Entity // mapeia a classe para o cliente da tabela
+@Table(name="customer")//a classe tem o mesmo nome da tabela
+
+public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // indica que esse campo é a chave primária e seu valor é gerado automaticamente. 
+
     private Long id;
- 
     private String name;
     private String email;
     private String address;
- 
+
     protected Customer() {
     }
- 
+
     protected Customer(String name, String email, String address) {
         this.name = name;
         this.email = email;
         this.address = address;
     }
-    
-    // getters and setters are not shown for brevity
 
-    public Long getId() {
-        return id;
-    }
+    //id
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    //name
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
 
-    public String getName() {
-        return name;
-    }
+    //email
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
+    //endereco
+    public String getAddress() {return address;}
+    public void setAddress(String address) {this.address = address;}
 }
